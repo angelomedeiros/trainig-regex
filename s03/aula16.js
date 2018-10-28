4c2f0c7351cea0b9855fc7893afbc8542c37d456
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-10-27 21:16:19 -0300

    Aula 13 e 16

---
+++ s03/aula16.js
@@ -0,0 +1,8 @@
+const texto = "1,2,3,4,5,6, :a.b c!d?e";
+
+const regexVirgula = /,/g; // ou /,/
+console.log(texto.split(regexVirgula));
+console.log(texto.match(regexVirgula));
+console.log(texto.match(/A/g));
+console.log(texto.match(/A/gi));
+console.log(texto.match(/a/i));
